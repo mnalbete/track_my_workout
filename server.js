25a31a392bbf9d0bc034e81c17600e66afd764af
mathew <mnalbete@uw.edu>
2021-03-25 19:22:36 -0700

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || 'mongodb://localhost/trackmyworkout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
